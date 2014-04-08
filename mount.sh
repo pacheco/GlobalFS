d6d1cf8cbc83af4e618f109f63a4d6bf4067549a
pacheco <contact@leandropacheco.com>
2014-04-08 22:14:47 +0200

    making mount.sh a little more flexible

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-JAVAFS_PATH="/Users/pacheco/usr/lib"
+JAVAFS_PATH="~/usr/lib"
 
 export CLASSPATH="./target/paxosfs-fuse-0.0.1-SNAPSHOT-jar-with-dependencies.jar":$CLASSPATH
 
-java -Djava.library.path=$JAVAFS_PATH ch.usi.paxosfs.client.PaxosFileSystem 2 127.0.0.1:2181 $@
+java -Djava.library.path=$JAVAFS_PATH ch.usi.paxosfs.client.PaxosFileSystem 2 $@
