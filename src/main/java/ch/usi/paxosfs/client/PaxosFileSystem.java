pacheco/GlobalFS

commit c07063c9ad349e7f4a58c4d41f29829add869368

Fixed global writes when one datacenter dies
- write succeeds if the block is written to at least 2 DCs (hardcoded)

src/main/java/ch/usi/paxosfs/client/PaxosFileSystem.java
@@ -6,6 +6,7 @@ import ch.usi.paxosfs.replica.ReplicaManager;
 import ch.usi.paxosfs.rpc.*;
 import ch.usi.paxosfs.storage.Storage;
 import ch.usi.paxosfs.storage.StorageFactory;
+import ch.usi.paxosfs.storage.StorageFuture;
 import ch.usi.paxosfs.util.UUIDUtils;
 import ch.usi.paxosfs.util.Utils;
 import fuse.*;
@@ -28,7 +29,6 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class PaxosFileSystem implements Filesystem3 {
@@ -506,7 +506,7 @@ public class PaxosFileSystem implements Filesystem3 {
 			Response r = client.readBlocks(path, handle, offset, (long) buf.remaining(), instanceMap.get());
 			instanceMap.get().putAll(r.instanceMap);
 			res = r.readBlocks;
-			List<Future<byte[]>> futureValues = new LinkedList<>();
+			List<StorageFuture<byte[]>> futureValues = new LinkedList<>();
 			// dispatch the requests
 			for (DBlock b : res.getBlocks()) {
 				if (b.getId().length != 0) {
@@ -515,10 +515,8 @@ public class PaxosFileSystem implements Filesystem3 {
 				}
 			}
 			// wait for completion
-			List<byte[]> values = new ArrayList<byte[]>(futureValues.size());
-            Iterator<DBlock> blocksIter = res.getBlocks().iterator(); // TODO: this iterator is required only for the debug message. Remove later?
-			for (Future<byte[]> f: futureValues) {
-                DBlock b = blocksIter.next();
+			List<byte[]> values = new ArrayList<>(futureValues.size());
+			for (StorageFuture<byte[]> f: futureValues) {
 				try {
 					values.add(f.get());
 				} catch (InterruptedException | ExecutionException e) {
@@ -531,7 +529,7 @@ public class PaxosFileSystem implements Filesystem3 {
 			Iterator<byte[]> valuesIter = values.iterator();
 			for (DBlock b : res.getBlocks()) {
 				if (b.getId().length == 0){ 
-					// zero block
+					// zeroed block
 					int size = b.getEndOffset() - b.getStartOffset();
 					buf.put(new byte[size], 0, size);
 				} else { 
@@ -567,12 +565,12 @@ public class PaxosFileSystem implements Filesystem3 {
         writeCount.incrementAndGet();
         opCount.incrementAndGet();
 		FileHandle handle = (FileHandle) fh;
-		Set<Byte> allPartitions = this.partitionOracle.partitionsOf(path);
+		Set<Byte> allPartitions = this.partitionOracle.partitionsOf(path); // partitions to store the data block
 		int partition = choosePartition(allPartitions).intValue(); // partition to send the request
 		FuseOps.Client client = getClient((byte) partition);
 		try {
 			List<DBlock> blocks = new LinkedList<>();
-			List<Future<Boolean>> putFutures = new LinkedList<>();
+			List<StorageFuture<Boolean>> putFutures = new LinkedList<>();
 
 			while (buf.remaining() >= MAXBLOCKSIZE) {
 				byte[] data = new byte[MAXBLOCKSIZE];
@@ -601,19 +599,23 @@ public class PaxosFileSystem implements Filesystem3 {
             Iterator<DBlock> blocksIter = blocks.iterator();
             int blocksIterAux = 1; // there are more futures than blocks because of replication. This var is used to control when .next() is called.
             DBlock b = null;
-			for (Future<Boolean> put : putFutures) {
+			for (StorageFuture<Boolean> put : putFutures) {
                 if (blocksIterAux % allPartitions.size() == 1) { b = blocksIter.next(); }
                 blocksIterAux++;
-
 				try {
 					if (!put.get()) {
-                        log.error("Error writing data block " + new String(b.getId()));
-                        throw new FSError(Errno.EREMOTEIO, "Error storing data block!");
+                        b.getStorage().remove(Byte.valueOf(put.getPartition()));
+                        log.error("Error writing data block to storage " + put.getPartition());
 					}
 				} catch (InterruptedException | ExecutionException e) {
+                    b.getStorage().remove(Byte.valueOf(put.getPartition()));
 					e.printStackTrace();
-					throw new FSError(Errno.EREMOTEIO, "Error storing data block!");
+                    log.error("Error writing data block to storage " + put.getPartition());
 				}
+                if (b.getStorage().size() < 2) {
+                    // FIXME: TODO: hardcoded replication level
+                    throw new FSError(Errno.EREMOTEIO, "Error storing data block: Required replication not possible");
+                }
 			}
 			Response r = client.writeBlocks(path, handle, offset, blocks, instanceMap.get());
 			instanceMap.get().putAll(r.instanceMap);

src/main/java/ch/usi/paxosfs/replica/FileSystemReplica.java
@@ -343,22 +343,25 @@ public class FileSystemReplica implements Runnable {
 		if (n.isDir()) {
             throw new FSError(FuseException.EISDIR, "Is a directory");
         }
-					/*
-					 * Flags from open(2) - already removed flags the Fuse docs
-					 * say are not passed on
-					 *
-					 * O_RDONLY open for reading only O_WRONLY open for writing
-					 * only O_RDWR open for reading and writing O_NONBLOCK do
-					 * not block on open or for data to become available
-					 * O_APPEND append on each write O_TRUNC truncate size to 0
-					 * O_SHLOCK atomically obtain a shared lock O_EXLOCK
-					 * atomically obtain an exclusive lock O_NOFOLLOW do not
-					 * follow symlinks O_SYMLINK allow open of symlinks
-					 * O_EVTONLY descriptor requested for event notifications
-					 * only O_CLOEXEC mark as close-on-exec
-					 */
-		log.debug("Flags " + Integer.toHexString(open.getFlags()));
-		FileHandle fh = new FileHandle(c.getReqId(), open.getFlags(), localPartition.byteValue());
+        /*
+         * Flags from open(2) - already removed flags the Fuse docs
+		 * say are not passed on
+		 *
+		 * O_RDONLY open for reading only
+		 * O_WRONLY open for writing only
+		 * O_RDWR open for reading and writing
+		 * O_NONBLOCK do not block on open or for data to become available
+		 * O_APPEND append on each write
+		 * O_TRUNC truncate size to 0
+		 * O_SHLOCK atomically obtain a shared lock
+		 * O_EXLOCK atomically obtain an exclusive lock
+		 * O_NOFOLLOW do not follow symlinks
+		 * O_SYMLINK allow open of symlinks
+		 * O_EVTONLY descriptor requested for event notifications only
+		 * O_CLOEXEC mark as close-on-exec
+		 */
+        log.debug("Flags " + Integer.toHexString(open.getFlags()));
+        FileHandle fh = new FileHandle(c.getReqId(), open.getFlags(), localPartition.byteValue());
 		this.openFiles.put(Long.valueOf(fh.getId()), (FileNode) n);
 
 		if (c.getInvolvedPartitions().size() > 1) {

src/main/java/ch/usi/paxosfs/storage/DecidedStorageFuture.java
@@ -8,10 +8,15 @@ import java.util.concurrent.TimeoutException;
 /**
  * Used when a future is needed but its value is already available/decided.
  */
-class DecidedFuture<T> implements Future<T> {
-    private T theValue;
-    public DecidedFuture(T value) {
+class DecidedStorageFuture<T> implements StorageFuture<T> {
+    private final T theValue;
+    private final byte thePartition;
+    private final byte[] theKey;
+
+    public DecidedStorageFuture(final byte partition, final byte[] key, final T value) {
         theValue = value;
+        theKey = key;
+        thePartition = partition;
     }
 
     @Override
@@ -39,5 +44,14 @@ class DecidedFuture<T> implements Future<T> {
             ExecutionException, TimeoutException {
         return theValue;
     }
-}
 
+    @Override
+    public byte getPartition() {
+        return thePartition;
+    }
+
+    @Override
+    public byte[] getKey() {
+        return theKey;
+    }
+}

src/main/java/ch/usi/paxosfs/storage/HttpStorage.java
@@ -16,7 +16,6 @@ import java.nio.file.Path;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class HttpStorage implements Storage {
 	private static final int TIMEOUT = 3000;
@@ -148,10 +147,10 @@ public class HttpStorage implements Storage {
     }
 
     @Override
-    public Future<Boolean> put(byte partition, byte[] key, byte[] value) {
+    public StorageFuture<Boolean> put(final byte partition, final byte[] key, final byte[] value) {
         String server = this.randomServer(Byte.valueOf(partition));
         if (server == null) { // unknown partition
-            return new DecidedFuture<>(false);
+            return new DecidedStorageFuture<>(partition, key, false);
         }
         final String keyStr = keyBytesToString(key);
         Request req = Request.Put(server + keyStr)
@@ -159,31 +158,31 @@ public class HttpStorage implements Storage {
                 .addHeader("Sync-Mode", "sync")
                 .connectTimeout(TIMEOUT)
                 .bodyByteArray(value);
-        return asyncHttp.execute(req, new PutHandler(keyStr, value));
+        return new StorageFutureWrapper<>(partition, key, asyncHttp.execute(req, new PutHandler(keyStr, value)));
     }
 
     @Override
-    public Future<byte[]> get(byte partition, byte[] key) {
-        String server = this.randomServer(Byte.valueOf(partition));
+    public StorageFuture<byte[]> get(final byte partition, final byte[] key) {
+        final String server = this.randomServer(Byte.valueOf(partition));
         if (server == null) { // unknown partition
-            return new DecidedFuture<>(null);
+            return new DecidedStorageFuture<>(partition, key, null);
         }
         // first look into the local cache
         final String keyStr = keyBytesToString(key);
         final byte[] value = cache.getIfPresent(keyStr);
         if (value != null) {
-            return new DecidedFuture<>(value);
+            return new DecidedStorageFuture<>(partition, key, value);
         }
         // go to the storage
         Request req = Request.Get(server + keyStr)
                 .addHeader("Content-Type", "application/octet-stream")
                 .addHeader("Sync-Mode", "sync")
                 .connectTimeout(TIMEOUT);
-        return asyncHttp.execute(req, new GetHandler(keyStr));
+        return new StorageFutureWrapper<>(partition, key, asyncHttp.execute(req, new GetHandler(keyStr)));
     }
 
     @Override
-    public Future<Boolean> delete(byte partition, byte[] key) {
+    public StorageFuture<Boolean> delete(final byte partition, final byte[] key) {
         // TODO not implemented
         throw new NotImplementedException();
     }

src/main/java/ch/usi/paxosfs/storage/LocalStorage.java
@@ -16,34 +16,34 @@ public class LocalStorage implements Storage {
     }
 
     @Override
-    public Future<Boolean> put(byte partition, byte[] key, byte[] value) {
+    public StorageFuture<Boolean> put(byte partition, byte[] key, byte[] value) {
         HashMap<String, byte[]> store = partitions.get(Byte.valueOf(partition));
         if (store == null) {
             store = new HashMap<>();
             partitions.put(Byte.valueOf(partition), store);
         }
         store.put(new String(key), value);
-        return new DecidedFuture<>(true);
+        return new DecidedStorageFuture<>(partition, key, true);
     }
 
     @Override
-    public Future<byte[]> get(byte partition, byte[] key) {
+    public StorageFuture<byte[]> get(byte partition, byte[] key) {
         HashMap<String, byte[]> store = partitions.get(Byte.valueOf(partition));
         byte[] value = null;
         if (store != null) {
             value = store.get(new String(key));
         }
-        return new DecidedFuture<>(value);
+        return new DecidedStorageFuture<>(partition, key, value);
     }
 
     @Override
-    public Future<Boolean> delete(byte partition, byte[] key) {
+    public StorageFuture<Boolean> delete(byte partition, byte[] key) {
         HashMap<String, byte[]> store = partitions.get(Byte.valueOf(partition));
         if (store != null) {
             if (store.remove(new String(key)) != null) {
-                return new DecidedFuture<>(true);
+                return new DecidedStorageFuture<>(partition, key, true);
             }
         }
-        return new DecidedFuture<>(false);
+        return new DecidedStorageFuture<>(partition, key, false);
     }
 }

src/main/java/ch/usi/paxosfs/storage/MemcachedStorage.java
@@ -79,25 +79,25 @@ public class MemcachedStorage implements Storage {
     }
 
     @Override
-    public Future<Boolean> put(byte partition, byte[] key, byte[] value) {
+    public StorageFuture<Boolean> put(byte partition, byte[] key, byte[] value) {
         MemcachedClient c = partitionClients.get(Byte.valueOf(partition));
         if (c == null) {
-            return new DecidedFuture<>(Boolean.FALSE);
+            return new DecidedStorageFuture<>(partition, key, Boolean.FALSE);
         }
-        return c.add(keyBytesToString(key), 0, value);
+        return new StorageFutureWrapper<>(partition, key, c.add(keyBytesToString(key), 0, value));
     }
 
     @Override
-    public Future<byte[]> get(byte partition, byte[] key) {
+    public StorageFuture<byte[]> get(byte partition, byte[] key) {
         MemcachedClient c = partitionClients.get(Byte.valueOf(partition));
         if (c == null) {
-            return new DecidedFuture<>(null);
+            return new DecidedStorageFuture<>(partition, key, null);
         }
-        return (Future<byte[]>)(Future<?>) c.asyncGet(keyBytesToString(key));
+        return new StorageFutureWrapper<>(partition, key, (Future<byte[]>)(Future<?>) c.asyncGet(keyBytesToString(key)));
     }
 
     @Override
-    public Future<Boolean> delete(byte partition, byte[] key) {
+    public StorageFuture<Boolean> delete(byte partition, byte[] key) {
         // TODO not implemented
         throw new NotImplementedException();
     }

src/main/java/ch/usi/paxosfs/storage/NullStorage.java
@@ -11,18 +11,18 @@ public class NullStorage implements Storage {
     }
 
     @Override
-    public Future<Boolean> put(byte partition, byte[] key, byte[] value) {
-        return new DecidedFuture<>(Boolean.TRUE);
+    public StorageFuture<Boolean> put(byte partition, byte[] key, byte[] value) {
+        return new DecidedStorageFuture<>(partition, key, Boolean.TRUE);
     }
 
     @Override
-    public Future<byte[]> get(byte partition, byte[] key) {
-        return new DecidedFuture<>(data);
+    public StorageFuture<byte[]> get(byte partition, byte[] key) {
+        return new DecidedStorageFuture<>(partition, key, data);
     }
 
     @Override
-    public Future<Boolean> delete(byte partition, byte[] key) {
-        return new DecidedFuture<>(Boolean.TRUE);
+    public StorageFuture<Boolean> delete(byte partition, byte[] key) {
+        return new DecidedStorageFuture<>(partition, key, Boolean.TRUE);
     }
 
 }

src/main/java/ch/usi/paxosfs/storage/Storage.java
@@ -24,7 +24,7 @@ public interface Storage {
      * @param value
      * @return
      */
-    Future<Boolean> put(byte partition, byte[] key, byte[] value);
+    StorageFuture<Boolean> put(byte partition, byte[] key, byte[] value);
 
     /**
      * Fetch data related to a key in a given partition. Future.get() returns null if the key does not exist
@@ -33,7 +33,7 @@ public interface Storage {
      * @param key
      * @return
      */
-    Future<byte[]> get(byte partition, byte[] key);
+    StorageFuture<byte[]> get(byte partition, byte[] key);
 
     /**
      * Delete data related to a key in a given partition
@@ -42,5 +42,5 @@ public interface Storage {
      * @param key
      * @return
      */
-    Future<Boolean> delete(byte partition, byte[] key);
+    StorageFuture<Boolean> delete(byte partition, byte[] key);
 }

commit e557fa0f53fc00269b9ef988b1f5f43cf889860a

Fixing a bug introduced by c07063c9ad349e7f4a58c4d41f29829add869368

src/main/java/ch/usi/paxosfs/client/PaxosFileSystem.java
@@ -594,29 +594,33 @@ public class PaxosFileSystem implements Filesystem3 {
 				}
 				blocks.add(b);
 			}
-			
-			// Check all puts were successful
-            Iterator<DBlock> blocksIter = blocks.iterator();
-            int blocksIterAux = 1; // there are more futures than blocks because of replication. This var is used to control when .next() is called.
-            DBlock b = null;
-			for (StorageFuture<Boolean> put : putFutures) {
-                if (blocksIterAux % allPartitions.size() == 1) { b = blocksIter.next(); }
-                blocksIterAux++;
-				try {
-					if (!put.get()) {
-                        b.getStorage().remove(Byte.valueOf(put.getPartition()));
-                        log.error("Error writing data block to storage " + put.getPartition());
-					}
-				} catch (InterruptedException | ExecutionException e) {
-                    b.getStorage().remove(Byte.valueOf(put.getPartition()));
-					e.printStackTrace();
-                    log.error("Error writing data block to storage " + put.getPartition());
-				}
-                if (b.getStorage().size() < 2) {
-                    // FIXME: TODO: hardcoded replication level
-                    throw new FSError(Errno.EREMOTEIO, "Error storing data block: Required replication not possible");
+
+
+            // Check that puts were successful
+            Iterator<StorageFuture<Boolean>> futuresIter = putFutures.iterator();
+            for (DBlock b: blocks) {
+                // check each partition write
+                for (Byte p : allPartitions) {
+                    StorageFuture<Boolean> putFuture = futuresIter.next();
+                    boolean ok = false;
+                    try {
+                        ok = putFuture.get();
+                    } catch (InterruptedException | ExecutionException e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (!ok) {
+                            b.getStorage().remove(Byte.valueOf(p));
+                            log.error("Error writing data block to storage " + p);
+                        }
+                    }
                 }
-			}
+
+                // FIXME: TODO: how to handle a replicated write? Right now we assume all replicated writes are for (3 partitions), writing to 2 is enough
+                if (allPartitions.size() == 3 && b.getStorage().size() < 2) {
+                    throw new FSError(Errno.EREMOTEIO, "Error storing data block: Required replication not achieved");
+                }
+            }
+
 			Response r = client.writeBlocks(path, handle, offset, blocks, instanceMap.get());
 			instanceMap.get().putAll(r.instanceMap);
 			returnClient(client, (byte) partition);