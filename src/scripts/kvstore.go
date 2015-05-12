8bd4110f237faf7f816b4d55778069b1e3821692
pacheco <le.pacheco@gmail.com>
2015-05-12 16:32:48 +0200

    Go dht implementation

---
+++ src/scripts/kvstore.go
@@ -0,0 +1,282 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type HostPort struct {
+	Host string
+	Port int
+}
+
+func (hp *HostPort) AsString() string {
+	return hp.Host + ":" + strconv.FormatInt(int64(hp.Port), 10)
+}
+
+func (hp *HostPort) PortString() string {
+	return strconv.FormatInt(int64(hp.Port), 10)
+}
+
+// replication config
+var selfId int
+var replication int
+var replicas = make([]HostPort, 0)
+
+// key value store
+var storeLock sync.Mutex // RWMutex made no difference
+var store = make(map[string][]byte)
+
+// From the docs, this can be shared by all goroutines.
+var sharedClient = &http.Client{}
+
+func parseConfig(file string) {
+	f, err := os.Open(file)
+	if err != nil {
+		panic("ERROR: opening config file")
+	}
+	// read file line by line
+	scanner := bufio.NewScanner(bufio.NewReader(f))
+	scanner.Split(bufio.ScanLines)
+	for {
+		if !scanner.Scan() {
+			if scanner.Err() != nil {
+				continue
+			} else {
+				break
+			}
+		}
+		line := scanner.Text()
+		values := strings.Fields(line)
+		// skip empty lines
+		if len(values) == 0 {
+			continue
+		}
+		switch values[0] {
+		case "replication":
+			rep, err := strconv.ParseInt(values[2], 0, 32)
+			replication = int(rep)
+			if err != nil {
+				panic("ERROR: parsing config file")
+			}
+		default:
+			port, err := strconv.ParseInt(values[1], 0, 32)
+			if err != nil {
+				panic("ERROR: parsing config file")
+			}
+			// FIXME: lua dht does HTTP on config port + 1
+			replicas = append(replicas, HostPort{values[0], int(port) + 1})
+		}
+	}
+}
+
+func storagesForKey(keyhash uint32) []HostPort {
+	n := len(replicas)
+	pos := int(keyhash) % n
+	switch replication {
+	case 0:
+		panic("replication should be > 0")
+	case 1:
+		return []HostPort{replicas[pos]}
+	case 2:
+		return []HostPort{replicas[pos], replicas[(pos+1)%n]}
+	case 3:
+		return []HostPort{replicas[pos], replicas[(pos+1)%n], replicas[(pos+2)%n]}
+	default:
+		panic("replication number not supported")
+	}
+}
+
+// handle GET for a key. Fetches the key from the correct replica if
+// the key is not local.
+func handleGet(w http.ResponseWriter, key string, keyhash uint32) {
+	storages := storagesForKey(keyhash)
+	status := 200
+	isLocal := false
+	val := []byte{}
+
+	for _, st := range storages {
+		if st == replicas[selfId] {
+			isLocal = true
+		}
+	}
+
+	// handle get locally if possible
+	if isLocal {
+		ok := false
+		storeLock.Lock()
+		val, ok = store[key]
+		storeLock.Unlock()
+		if !ok {
+			status = 404
+		}
+	} else {
+		// remote get
+		c := sharedClient
+		st := storages[0]
+		url := "http://" + st.AsString() + "/" + key
+		resp, err := c.Get(url)
+		defer resp.Body.Close()
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+		status = resp.StatusCode
+		val, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+	}
+
+	// reply
+	if status == 200 {
+		w.Write(val)
+	} else {
+		w.WriteHeader(status)
+	}
+}
+
+// used as a goroutine for async remote put (POST)
+func remotePut(putUrl string, data []byte, statusc chan int, errc chan error) {
+	req, err := http.NewRequest("POST", putUrl, bytes.NewReader(data))
+	if err != nil {
+		errc <- err
+		return
+	}
+	c := sharedClient
+	resp, err := c.Do(req)
+	resp.Body.Close()
+	if err != nil {
+		errc <- err
+		return
+	}
+	statusc <- resp.StatusCode
+}
+
+// handle PUT for a key. Will replicate by sending a POST for the key
+// on other replicas, given the "replication" factor (0 means a single
+// replica, 1 means 2 and so on).
+func handlePut(w http.ResponseWriter, r *http.Request, key string, keyhash uint32) {
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	storages := storagesForKey(keyhash)
+	isLocal := false
+	status := 200
+
+	rc := make(chan int, len(storages))
+	errc := make(chan error, len(storages))
+	for _, st := range storages {
+		if st == replicas[selfId] {
+			// local put
+			isLocal = true
+		} else {
+			// remote put
+			go remotePut("http://"+st.AsString()+"/"+key, data, rc, errc)
+		}
+	}
+	// store locally
+	if isLocal {
+		storeLock.Lock()
+		if _, exists := store[key]; exists {
+			status = 409
+		} else {
+			store[key] = data
+		}
+		storeLock.Unlock()
+	}
+	// return early on error
+	if status != 200 {
+		w.WriteHeader(status)
+		return
+	}
+	// check remotes are ok
+	remotes := len(storages)
+	if isLocal {
+		remotes--
+	}
+	for i := 0; i < remotes; i++ {
+		rStatus := 500
+		var err error = nil
+		select {
+		case err = <-errc:
+		case rStatus = <-rc:
+		}
+		// return early on error
+		if err != nil || rStatus != 200 {
+			w.WriteHeader(rStatus)
+			return
+		}
+	}
+	w.WriteHeader(200)
+	return
+}
+
+// Replication is done by sending POST to other replicas. Handles the POST by storing locally only.
+func handlePutLocal(w http.ResponseWriter, r *http.Request, key string, keyhash uint32) {
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	storeLock.Lock()
+	if _, exists := store[key]; exists {
+		w.WriteHeader(409)
+	} else {
+		store[key] = data
+	}
+	storeLock.Unlock()
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Path[1:]
+	// hash key
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	keyhash := h.Sum32()
+	// find storage
+	switch r.Method {
+	case "GET":
+		// fmt.Println("GET", key)
+		handleGet(w, key, keyhash)
+	case "PUT":
+		// fmt.Println("PUT", key)
+		handlePut(w, r, key, keyhash)
+	case "POST":
+		// fmt.Println("POST", key)
+		handlePutLocal(w, r, key, keyhash)
+	default:
+		fmt.Println("UNSUPPORTED")
+		w.WriteHeader(405)
+	}
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		panic("usage: kvstore <cfgfile> <id>")
+	}
+	id, err := strconv.ParseInt(os.Args[2], 0, 32)
+	if err != nil {
+		panic("self id must be an integer")
+	}
+	selfId = int(id)
+	// read config
+	parseConfig(os.Args[1])
+	// serve
+	hostString := replicas[selfId].AsString()
+	fmt.Println("replication = ", replication)
+	fmt.Println("Serving HTTP at:", hostString)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":"+replicas[selfId].PortString(), nil)
+}
